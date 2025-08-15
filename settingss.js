a3b2c456ebb59d5b8718c4981abe2b7f05d9b0b0
frankboi548-byte <frankboi548@gmail.com>
2025-08-15 05:48:55 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~07c00ZrL#ey0dsWICby1URVtKFN2yIv67w93Rpei8muULxbpYn08",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "27783583068", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "SouthAfrica/Capetown" //put your country timeZone....leave blank if u don't know.
 };
